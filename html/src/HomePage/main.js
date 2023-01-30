25179889fadf54b0b6995fe9621626ce295e3ff6
valereon <tools418@gmail.com>
2023-01-30 14:13:56 +0000

    daily push

--- html/src/HomePage/main.js
+++ html/src/HomePage/main.js
@@ -3,3 +3,19 @@
 
 
 
+function hovered(id){
+    var hover = document.getElementById("hover");
+    hover.innerHTML = id;
+}
+function unhovered(){
+    var hover = document.getElementById("hover");
+    hover.innerHTML = "";
+}
+
+
+
+
+function home(){
+    window.location.href = "/pages/insideOfHome.html";
+
+}
