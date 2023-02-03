661552a3dea1303986628cb0ec605715214153e3
valereon <tools418@gmail.com>
2023-02-03 23:41:19 +0000

    commit for spencer

--- html/src/HomePage/addAscii.js
+++ html/src/HomePage/addAscii.js
@@ -1,9 +1,31 @@
-import { insideOfHouseAscii } from "../../images/AsciiArt.js";
+import * as ascii from "../../images/AsciiArt.js";
+
+
+
+function elementsToClick(tag, className, text, baseText){
+    
+    console.log(text)
+    text = text.split("\n")
+    console.log(text)
+    for(let i = 0; i < text.length; i++){
+        text[i] = text[i].trimEnd();
+        baseText.innerHTML = baseText.innerHTML.replace(text[i], `<${tag} class="${className}">${text[i]}</${tag}>`)
+
+    }    
+
+    return baseText;
+
+};
 
 
 window.onload = function(){
-    console.log("loaded");
-    console.log(insideOfHouseAscii)
+    
+    let textFormattedEle = document.createElement("pre");
+    let spanFix = document.createElement("span");
+    textFormattedEle.className = "ascii centered";
+    textFormattedEle.innerText = ascii.insideOfHouseAscii;
+    let product = elementsToClick("span", "ascii", ascii.swordInHouse, textFormattedEle);
+    document.body.append(product);
 };
 
 
